aa2bfcdd28de90186ac05b9e355952d982454e2a
Ruben London Met <rubenswork20@gmail.com>
2022-10-03 10:33:18 +0100

    Updating files to work with v142 build tools

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,10 @@
 #include<iostream>
 #include<string>
 #include<SDL.h>
-#include<windows.h>
-#include<windows.system.diagnostics.h>
+#include<SDL_net.h>
 
 int main(int argc, char* argv[])
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
-
-//	MEMORYSTATUSEX;
-
 	std::cout << "Test Run" << std::endl;
-	//std::cout << info << std::endl;
 	return 0;
-
 }
